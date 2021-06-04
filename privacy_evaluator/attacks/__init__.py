394670c48c1dff13227c89c3623412759fd91c41
Jakob Heinrich <reg@jakobheinrich.de>
2021-06-04 18:05:22 +0200

    Style (package-installation): Switch to local imports.
    See !112.
    Solves #111.

--- privacy_evaluator/attacks/__init__.py
+++ privacy_evaluator/attacks/__init__.py
@@ -1,8 +1,14 @@
 """
 Module providing attacks.
 """
+<<<<<<< HEAD
 
 from privacy_evaluator.attacks import membership_inference
 from privacy_evaluator.attacks.attack import Attack
 from privacy_evaluator.attacks.sample_attack import SampleAttack
 from privacy_evaluator.attacks.property_inference_attack import PropertyInferenceAttack
+=======
+from . import membership_inference
+from .attack import Attack
+from .sample_attack import SampleAttack
+>>>>>>> Style (package-installation): Switch to local imports.
