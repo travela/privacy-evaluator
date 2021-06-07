af06a53e2ad8a02e8e915b577608da65278fd284
marisanest <marisanest@mailbox.org>
2021-06-07 19:35:26 +0200

    Fix merge issues and failing tests.

--- tests/attacks/property_inference_test.py
+++ tests/attacks/property_inference_test.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from privacy_evaluator.attacks.property_inference_attack import PropertyInferenceAttack
 from privacy_evaluator.classifiers.classifier import Classifier
 from privacy_evaluator.utils.data_utils import (
@@ -22,6 +23,8 @@ def test_property_inference_attack():
 =======
 import pytest
 
+=======
+>>>>>>> Fix merge issues and failing tests.
 from privacy_evaluator.attacks.property_inference_attack import PropertyInferenceAttack
 from privacy_evaluator.classifiers.classifier import Classifier
 from privacy_evaluator.models.train_cifar10_torch.data import (
@@ -33,7 +36,7 @@ from privacy_evaluator.models.train_cifar10_torch.train import trainer_out_model
 
 def test_property_inference_attack():
     train_dataset, test_dataset = dataset_downloader()
-    input_shape = [32, 32, 3]
+    input_shape = (32, 32, 3)
     num_classes = 2
     num_elements_per_classes = {0: 5000, 1: 5000}
 
@@ -47,11 +50,17 @@ def test_property_inference_attack():
 >>>>>>> Team2sprint2 (#102)
 
     # change pytorch classifier to art classifier
-    target_model = Classifier._to_art_classifier(model, num_classes, input_shape)
+    target_model = Classifier(
+        classifier=model, loss="bla", nb_classes=num_classes, input_shape=input_shape
+    )
 
+<<<<<<< HEAD
 <<<<<<< HEAD
     attack = PropertyInferenceAttack(target_model, train_dataset)
 =======
     attack = PropertyInferenceAttack(target_model)
 >>>>>>> Team2sprint2 (#102)
+=======
+    attack = PropertyInferenceAttack(target_model.to_art_classifier)
+>>>>>>> Fix merge issues and failing tests.
     attack.attack()
