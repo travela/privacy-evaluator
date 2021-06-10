c183b199accfdcec616e58acc0f12b3c488233d1
florian <florian.mies@fu-berlin.de>
2021-06-10 15:46:34 +0200

    jetzt aber

--- privacy_evaluator/attacks/__init__.py
+++ privacy_evaluator/attacks/__init__.py
@@ -1,21 +1,7 @@
 """
 Module providing attacks.
 """
-<<<<<<< HEAD
 
-from privacy_evaluator.attacks import membership_inference
-from privacy_evaluator.attacks.attack import Attack
-from privacy_evaluator.attacks.sample_attack import SampleAttack
-from privacy_evaluator.attacks.property_inference_attack import PropertyInferenceAttack
-<<<<<<< HEAD
-=======
 from . import membership_inference
 from .attack import Attack
-<<<<<<< HEAD
-from .sample_attack import SampleAttack
->>>>>>> Style (package-installation): Switch to local imports.
-=======
->>>>>>> Team2sprint2 (#102)
-=======
 from .property_inference_attack import PropertyInferenceAttack
->>>>>>> Remove all sample classes and tests.
